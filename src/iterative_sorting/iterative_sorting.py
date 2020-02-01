af72f409795a0d5f7ceb5425964bb8b3dc9c12ef
Tevvin <Tevvin7395@icloud.com>
2020-02-01 09:16:30 -0500

    bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,29 @@
 # TO-DO: Complete the selection_sort() function below
-def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+# def selection_sort(arr):
+# loop through n-1 elements
+# for i in range(0, len(arr) - 1):
+#cur_index = i
+#smallest_index = cur_index
 
-        # TO-DO: find next smallest element
-        for j in range(curr_index, len(arr)):
-            if arr[smallest_index] > arr[j]:
-                smallest_index = j
+# TO-DO: find next smallest element
+# for j in range(curr_index, len(arr)):
+# if arr[smallest_index] > arr[j]:
+#smallest_index = j
 
-        # TO-DO: swap
-        temp = arr[smallest_index]
-        arr[smallest_index] = arr[curr_index]
-        arr[cur_index] = temp
+# TO-DO: swap
+#temp = arr[smallest_index]
+#arr[smallest_index] = arr[curr_index]
+#arr[cur_index] = temp
 
-    return arr
+# return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
 
